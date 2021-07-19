e7a79f43e9077f9538639cdcac341d6b44111e56
Cole Rabson <colerabson@gmail.com>
2021-07-19 12:08:03 -0700

    started R6 class

---
+++ Narx.R
@@ -0,0 +1,25 @@
+library(R6)
+
+NeuralNet <- R6Class(classname = "NeuralNet", list(
+  ## Declare constants
+  learnrate <- 1,
+  randomConstraint <- 1,
+  epochs <- 1,
+  layers <- list("Learning" = list(NULL, NULL), "Validation" = list(NULL, NULL), "Test" = list(NULL, NULL)),
+  allInputs <- list(),
+  allTargets <- list(),
+  timeDelay <- list(), 
+  allResults <- list(),
+  mse <- list(),
+  mseAccum <- list(), 
+  validationTargetsActivations <- list(),
+  testTargetsActivations <- list(),
+  haltOnExtremes <- FALSE,
+  inputLayer <- NULL,
+  outputLayer <- NULL,
+  
+  initialize = function(inputNodes, totalHiddenNodesList, outputNodes, recurrentMods) {
+    
+  }
+  
+))
