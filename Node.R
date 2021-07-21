88676a40f49b8fead33c8ded9f137b3dd0307613
ajgallegos <A_J_Gallegos@protonmail.com>
2021-07-21 16:04:19 -0500

    Reformatted code

--- Node.R
+++ Node.R
@@ -226,183 +226,201 @@ Node <- R6Class(
 )
 
 ## Create the CopyNode Class
-CopyNode <- R6Class(classname = "CopyNode", inherit = Node, public = list(
-  ## initialize all necessary class variables
-  initialize = function(){
-    copy <- Node$new()
-    copy$node_type <- NODE_COPY
-    self$source_node_ <- NULL
-    self$source_type_ <- NULL
-    self$incoming_weight_ <- 1
-    self$existing_weight_ <- 0
-    copy$set_activation_type(ACTIVATION_LINEAR)
-  },
-  set_source_node = function(node){
-    ## Sets the source of previous recurrent values
-    self$source_node_ = node
-    invisible(self)
-  },
-  ## return the source of previous recurrent values
-  get_source_node = function(){
-    self$source_node_
-  },
-  ## Transfers source node's value to copy node's value
-  load_source_value = function(){
-    if(self$source_type_ == 'a'){
-      value <- self$source_node_$activate()
-    }
-    else if(self$source_type_ == 'v'){
-      value <- self$source_node_$get_value()
-    }
-    else{
-      throw("Invalid Source Type.")
-    }
-    
-    copy$value_ <- copy$value_ * self$existing_weight_ + value * self$incoming_weight_
-  },
-  ## gets the type of source value to use
-  get_source_type = function(){
-    self$source_type_
-  },
-  # This function gets the value that will be multiplied times the incoming source value.
-  get_incoming_weight = function(){
-    self$incoming_weight_
-  },
-  ## This function gets the value that will be multiplied times the existing value.
-  get_existing_weight = function(){
-    self$existing_weight_
-  },
-  ## This function accepts parameters governing what the source information
-  ## is used, and how the incoming and existing values are discounted.
-  ## 
-  ## Source type can be either 'a' for the activation value or 'v' for the
-  ## summed input value.
-  ## 
-  ## By setting the existing weight to zero, and the incoming discount to
-  ## 1.0. An Elman style update takes place.
-  ## 
-  ## By setting the existing weight to some fraction of 1.0 such as .5, a
-  ## Jordan style update can take place.
-  source_update_config = function(source_type, incoming_weight, existing_weight){
-    if(source_type %in% c('a', 'v')){
-      self$source_type_ = source_type
-    }
-    else{
-      throw("Invalid source type. Valid types are 'a' or 'v'.")
-    }
-    
-    errormsg <- "The incoming weight must be numeric between 0 and 1."
-    if(!is.numeric(incoming_weight)){
-      throw(errormsg)
-    }
-    if(!between(incoming_weight, 0.0, 1.0)){
-      throw(errormsg)
-    }
-    self$incoming_weight_ <- incoming_weight
-    
-    errormsg <- "The existing weight must be numeric between 0 and 1."
-    if(!is.numeric(existing_weight)){
-      throw(errormsg)
-    }
-    if(!between(existing_weight, 0.0, 1.0)){
-      throw(errormsg)
-    }
-    self$existing_weight_ <- existing_weight
-    invisible(self)
-  }
-))
+CopyNode <-
+  R6Class(
+    classname = "CopyNode",
+    inherit = Node,
+    public = list(
+      ## initialize all necessary class variables
+      initialize = function() {
+        copy <- Node$new()
+        copy$node_type <- NODE_COPY
+        self$source_node_ <- NULL
+        self$source_type_ <- NULL
+        self$incoming_weight_ <- 1
+        self$existing_weight_ <- 0
+        copy$set_activation_type(ACTIVATION_LINEAR)
+      },
+      set_source_node = function(node) {
+        ## Sets the source of previous recurrent values
+        self$source_node_ = node
+        invisible(self)
+      },
+      ## return the source of previous recurrent values
+      get_source_node = function() {
+        self$source_node_
+      },
+      ## Transfers source node's value to copy node's value
+      load_source_value = function() {
+        if (self$source_type_ == 'a') {
+          value <- self$source_node_$activate()
+        }
+        else if (self$source_type_ == 'v') {
+          value <- self$source_node_$get_value()
+        }
+        else{
+          throw("Invalid Source Type.")
+        }
+        
+        copy$value_ <-
+          copy$value_ * self$existing_weight_ + value * self$incoming_weight_
+      },
+      ## gets the type of source value to use
+      get_source_type = function() {
+        self$source_type_
+      },
+      # This function gets the value that will be multiplied times the incoming source value.
+      get_incoming_weight = function() {
+        self$incoming_weight_
+      },
+      ## This function gets the value that will be multiplied times the existing value.
+      get_existing_weight = function() {
+        self$existing_weight_
+      },
+      ## This function accepts parameters governing what the source information
+      ## is used, and how the incoming and existing values are discounted.
+      ##
+      ## Source type can be either 'a' for the activation value or 'v' for the
+      ## summed input value.
+      ##
+      ## By setting the existing weight to zero, and the incoming discount to
+      ## 1.0. An Elman style update takes place.
+      ##
+      ## By setting the existing weight to some fraction of 1.0 such as .5, a
+      ## Jordan style update can take place.
+      source_update_config = function(source_type,
+                                      incoming_weight,
+                                      existing_weight) {
+        if (source_type %in% c('a', 'v')) {
+          self$source_type_ = source_type
+        }
+        else{
+          throw("Invalid source type. Valid types are 'a' or 'v'.")
+        }
+        
+        errormsg <-
+          "The incoming weight must be numeric between 0 and 1."
+        if (!is.numeric(incoming_weight)) {
+          throw(errormsg)
+        }
+        if (!between(incoming_weight, 0.0, 1.0)) {
+          throw(errormsg)
+        }
+        self$incoming_weight_ <- incoming_weight
+        
+        errormsg <-
+          "The existing weight must be numeric between 0 and 1."
+        if (!is.numeric(existing_weight)) {
+          throw(errormsg)
+        }
+        if (!between(existing_weight, 0.0, 1.0)) {
+          throw(errormsg)
+        }
+        self$existing_weight_ <- existing_weight
+        invisible(self)
+      }
+    )
+  )
 
 
 ## Create BiasNode
-BiasNode <- R6Class(classname = "BiasNode", inherit = ProtoNode, public = list(
-  initialize = function(){
-    ## initialize constants for use
-    bias <- ProtoNode$new()
-    bias$node_type <- NODE_BIAS
-    self$activated_ <- bias$value_
-  },
-  ## always returns activation which is 1
-  activate = function(value = NULL){
-    return(1.0)
-  },
-  ## Also returns 1 since activation of bias node is always 1
-  error_func = function(value = 1.0){
-    value = 1.0
-    value
-  }
-))
+BiasNode <-
+  R6Class(
+    classname = "BiasNode",
+    inherit = ProtoNode,
+    public = list(
+      initialize = function() {
+        ## initialize constants for use
+        bias <- ProtoNode$new()
+        bias$node_type <- NODE_BIAS
+        self$activated_ <- bias$value_
+      },
+      ## always returns activation which is 1
+      activate = function(value = NULL) {
+        return(1.0)
+      },
+      ## Also returns 1 since activation of bias node is always 1
+      error_func = function(value = 1.0) {
+        value = 1.0
+        value
+      }
+    )
+  )
 
 
 ## Create connection class
-Connection <- R6Class(classname = "Connection", public = list(
-  initialize = function(lower_node, upper_node, weight = 0){
-    ## Initialize all necessary constants
-    self$lower_node <- lower_node
-    self$upper_node <- upper_node
-    self$weight_ <- NULL
-    self$set_weight(weight)
-  },
-  ## This function sets the weight of the connection, which relates to
-  ## the impact that a lower node's activation will have on an upper node's
-  ## value.
-  set_weight = function(weight){
-    if(!is.numeric(weight)){
-      throw("Weight type is not numeric.")
+Connection <- R6Class(
+  classname = "Connection",
+  public = list(
+    initialize = function(lower_node, upper_node, weight = 0) {
+      ## Initialize all necessary constants
+      self$lower_node <- lower_node
+      self$upper_node <- upper_node
+      self$weight_ <- NULL
+      self$set_weight(weight)
+    },
+    ## This function sets the weight of the connection, which relates to
+    ## the impact that a lower node's activation will have on an upper node's
+    ## value.
+    set_weight = function(weight) {
+      if (!is.numeric(weight)) {
+        throw("Weight type is not numeric.")
+      }
+      else{
+        self$weight_ <- weight
+      }
+      invisible(self)
+    },
+    ## This function adds to the weight of the connection, which is
+    ## proportional to the impact that a lower node's activation will
+    ## have on an upper node's value.
+    add_weight = function(weight) {
+      if (!is.numeric(weight)) {
+        throw("Weight type is not numeric.")
+      }
+      else{
+        self$weight_ = self$weight_ + weight
+      }
+    },
+    ## This function sets the weight of the connection, which is relates to
+    ## the impact that a lower node's activation will have on an upper node's
+    ## value.
+    get_weight = function() {
+      self$weight_
     }
-    else{
-      self$weight_ <- weight
-    }
-    invisible(self)
-  },
-  ## This function adds to the weight of the connection, which is
-  ## proportional to the impact that a lower node's activation will
-  ## have on an upper node's value.
-  add_weight = function(weight){
-    if(!is.numeric(weight)){
-      throw("Weight type is not numeric.")
-    }
-    else{
-      self$weight_ = self$weight_ + weight
-    }
-  },
-  ## This function sets the weight of the connection, which is relates to
-  ## the impact that a lower node's activation will have on an upper node's
-  ## value.
-  get_weight = function(){
-    self$weight_
-  }
-))
+  )
+)
 
 ## math functions and derivatives
 
 ## calculate the sigmoid
-sigmoid <- function(value){
+sigmoid <- function(value) {
   sig <- (1 / (1 + exp(-value)))
   sig
 }
 
 ## calculate the derivative of the sigmoid
-sigmoid_derivative <- function(value){
+sigmoid_derivative <- function(value) {
   value * (1 - value)
 }
 
 ## calculate the tanh
-tanh <- function(value){
+tanh <- function(value) {
   tanh(value)
 }
 
 ## calcualte the derivative of the tanh
-tanh_derivative <- function(value){
-  1 - (tanh(value)^2)
+tanh_derivative <- function(value) {
+  1 - (tanh(value) ^ 2)
 }
 
 ## returns the value given to it
-linear <- function(value){
+linear <- function(value) {
   value
 }
 
 ## returns 1
-linear_derivative <- function(value){
+linear_derivative <- function(value) {
   value <- 1
   value
 }
